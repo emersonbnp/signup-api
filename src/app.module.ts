e86a027d269ff5a59d2d67b9be79b508b42099a8
GilmarBnp <mestrevii@hotmail.com>
2024-01-22 16:14:25 -0300

    added jenkinsfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
-import { UserRepository } from './user/repository/user.repository';
 import { SecurityModule } from './security/jwt.module';
 
 @Module({
   imports: [
-    UserRepository,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(
       `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGODB_HOST}`,
--- src/user/repository/user.repository.ts
+++ src/user/repository/user.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from '../schema/user.document';
